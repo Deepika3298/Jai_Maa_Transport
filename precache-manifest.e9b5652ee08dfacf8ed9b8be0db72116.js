7ff8b987092baf6e61b5f896a144196b8d4d3db8
Deepika3298 <deepika.v3298@gmail.com>
2024-02-15 00:15:01 +0530

    Updates

--- precache-manifest.e9b5652ee08dfacf8ed9b8be0db72116.js
+++ precache-manifest.e9b5652ee08dfacf8ed9b8be0db72116.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77954ac16a5c13d27dbbd32b5604205e",
+    "revision": "29f8c502d541cad42f7c3618cecdd9d8",
     "url": "./index.html"
   },
   {
-    "revision": "83fc286e2e6a79de5f5a",
+    "revision": "642a6a111c1290e1b672",
     "url": "./static/css/main.fcfc5ecd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8188a033.chunk.js"
   },
   {
-    "revision": "83fc286e2e6a79de5f5a",
-    "url": "./static/js/main.09d5fa26.chunk.js"
+    "revision": "642a6a111c1290e1b672",
+    "url": "./static/js/main.013acb59.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
